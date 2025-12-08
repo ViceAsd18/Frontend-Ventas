68da7802d12892cea0e11407a8fe0c149a20ec42
ViceAsd18 <vicen.ramirezduocuc.cl>
2025-12-08 11:02:46 -0300

    Subir .env

--- auth/api.ts
+++ auth/api.ts
@@ -1,13 +1,15 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://localhost:3000/api/v1",
+    baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
+
     if (token && config.headers) {
-    config.headers.Authorization = `Bearer ${token}`;
+        config.headers.Authorization = `Bearer ${token}`;
     }
+
     return config;
 });
